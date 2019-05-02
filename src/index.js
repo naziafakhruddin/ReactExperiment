4d821d48f755fc28b87c47db6d48633010399835
naziafakhruddin <naziafakhruddin@gmail.com>
2019-05-02 14:41:17 +0100

    Chapter 3 is finished

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class Head extends React.Component {
+  render() {
+    return <div className="head" />;
+  }
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Head />, document.getElementById("head-target"));
